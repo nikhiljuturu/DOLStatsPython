6988da49bc40cb568381db6ff09c8d7dc398789e
Nikhil Juturu <nikhil89reddy@gmail.com>
2022-07-04 15:23:21 -0700

    Update dol_labor.py

--- dol_labor.py
+++ dol_labor.py
@@ -3,15 +3,15 @@ import urllib2
 from twilio.rest import TwilioRestClient
 
 #twilio client messages
-account_sid = "AC619397348359d64b87add3c99e73c510"
-auth_token  = "35047d0c66b01a949f48ac95275c30a6"
+account_sid = "xxxxxxxxxxx" #enter account SID here
+auth_token  = "xxxxxxxxxxx" #enter auth token here
 client = TwilioRestClient(account_sid, auth_token)
 
 #initializing the counter for finding my A-number
 finder = 0
 
 #trying to search for the value of the A-number in the webpage
-page = "http://dolstats.com/company/profile/CW%2BPROFESSIONAL%2BSERVICES%2BDBA%2BLOCHBRIDGE"
+page = "http://dolstats.com/company/profile/"
 linked = urllib2.urlopen(page)
 data = linked.read()
 
@@ -25,8 +25,8 @@ else:
 
 if (finder==1):
     message = client.messages.create(body="Your Labor is approved!! Congrats!",
-        to="+18322943791",    # Replace with your phone number
-        from_="+18635883725") # Replace with your Twilio number
+        to="+1XXXXXXXXXX",    # Replace with your phone number
+        from_="+1XXXXXXXXXX") # Replace with your Twilio number
     print message.sid
     webbrowser.open("https://www.youtube.com/watch?v=bUP5H3c02Wk")
 
